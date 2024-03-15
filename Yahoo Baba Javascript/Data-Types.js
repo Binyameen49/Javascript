75df2d5cd5d028a7aee648857eaf646b0a136647
Muhammad Binyameen <mbinyameen49fsd@gmail.com>
2024-03-15 21:43:25 +0500

    15.03.2024

---
+++ Yahoo Baba Javascript/Data-Types.js
@@ -0,0 +1,23 @@
+// Types of Data
+
+
+
+// var a = "85";
+// var b = 85;
+// var c = true;
+// var d = ["html", "css"];
+// var e = {first:"85", second:"85"};
+// var f;
+
+
+// document.write(typeof(a));
+// document.write("</br>");
+// document.write(typeof(b));
+// document.write("</br>");
+// document.write(typeof(c));
+// document.write("</br>");
+// document.write(typeof(d));
+// document.write("</br>");
+// document.write(typeof(e));
+// document.write("</br>");
+// document.write(typeof(f));
---
+++ Yahoo Baba Javascript/Math-Table.js
@@ -0,0 +1,22 @@
+let num1 = 2;
+
+for (let index = 1; index <= 10; index++) {
+
+        if (index != 5) {
+
+            console.log(`${num1} * ${index} = ${num1 * index}`)
+
+        }
+
+}
+
+
+// let able;
+// able = 5;
+
+
+// for (let index = 1; index <= 10; index++) {
+//     if (index != 5) {
+//         console.log(`${able} * ${index} = ${able * index}`);
+//     }
+// }
